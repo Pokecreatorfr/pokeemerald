58fd01100a24998413bde4861ac2ff22c488deaa
Mathéo Vignaud <pokecratorfr@gmail.com>
2022-04-30 17:52:01 +0200

    Changed the function that draws door animations.

--- src/field_camera.c
+++ src/field_camera.c
@@ -244,7 +244,15 @@ static void DrawMetatile(s32 metatileLayerType, u16 *tiles, u16 offset)
 {
     switch (metatileLayerType)
     {
-    case METATILE_LAYER_TYPE_SPLIT:
+    case 3:
+        gOverworldTilemapBuffer_Bg1[offset] = tiles[0];
+        gOverworldTilemapBuffer_Bg1[offset + 1] = tiles[1];
+        gOverworldTilemapBuffer_Bg1[offset + 0x20] = tiles[2];
+        gOverworldTilemapBuffer_Bg1[offset + 0x21] = tiles[3];
+
+
+        break;
+    case 2: // LAYER_TYPE_
         // Draw metatile's bottom layer to the bottom background layer.
         gOverworldTilemapBuffer_Bg3[offset] = tiles[0];
         gOverworldTilemapBuffer_Bg3[offset + 1] = tiles[1];
